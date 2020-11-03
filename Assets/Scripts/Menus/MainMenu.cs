5060c7f94d582a7694e9be972f48d21d7d720170
jjlebow@gmail.com <jlebow@uci.edu>
2020-11-02 16:58:30 -0800

    Added Functionality to main menu and load levels
    added ability to play game and choose level as well as added an options panel that can be cancelled out of

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -13,6 +13,8 @@ public class MainMenu : MonoBehaviour
 	public GameObject credits;
 	public GameObject quit;
 
+    public GameObject optionsPanel;
+
 	private TMP_Text text;
 
 	void Awake()
@@ -44,6 +46,18 @@ public class MainMenu : MonoBehaviour
     	LeanTween.moveX(options, -6.5f,1.5f).setEase(LeanTweenType.easeOutBack).setDelay(1.1f);
     	LeanTween.moveX(credits, -6f, 1.5f).setEase(LeanTweenType.easeOutBack).setDelay(1.2f);
     	LeanTween.moveX(quit, -5.5f, 1.5f).setEase(LeanTweenType.easeOutBack).setDelay(1.3f);
+    }
+
+    public void OpenSubMenu(GameObject menu)
+    {
+        LeanTween.scale(menu, new Vector3(0.25f, 0.5f, 0.5f), 0.5f).setEase(LeanTweenType.easeInCubic);
+        LeanTween.move(menu, new Vector3(0f, 0f, 0f), 0.5f);
+    }
+
+    public void HideOptionsMenu()
+    {
+        LeanTween.scale(optionsPanel, new Vector3(0f,0f,0f), 1f).setEase(LeanTweenType.easeOutCubic);
+        LeanTween.move(optionsPanel, new Vector3(-6.5f, -2.5f, 0), 1f);
     }
 
     void UpdateValueCallback(Color val)
